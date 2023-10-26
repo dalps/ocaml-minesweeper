2035b0763a5d2b782cbfa36362a9209b942b2830
Federico <dalpio23@gmail.com>
2023-10-26 23:50:04 +0200

    Cell unsealing

--- lib/main.ml
+++ lib/main.ml
@@ -1,12 +1,15 @@
 module T = ANSITerminal
 
-type cell = Bomb | Free of int
+type cell = Mined | Safe of int
+type cell_state = New of cell | Unsealed of cell | Sealed of cell
 type world = cell list list
+type field = cell_state list list
+type game_state = Over | Win | Continue
 
 let string_of_cell = function
-  | Bomb -> "   "
-  | Free 0 -> "   "
-  | Free n -> " " ^ string_of_int n ^ " "
+  | Mined -> "   "
+  | Safe 0 -> "   "
+  | Safe n -> " " ^ string_of_int n ^ " "
 
 let string_of_world w =
   List.fold_left
@@ -14,14 +17,17 @@ let string_of_world w =
       s ^ "\n" ^ List.fold_left (fun s c -> s ^ string_of_cell c) "" r)
     "" w
 
-let is_bomb = function Bomb -> true | _ -> false
+let is_bomb = function Mined -> true | _ -> false
 
-let peek (w : world) i j =
+let peek w i j =
   match (i, j) with
   | _ when i < 0 || j < 0 -> None
   | _ -> (
       let row = List.nth_opt w i in
       match row with None -> None | Some r -> List.nth_opt r j)
+
+let update f w = List.map (List.map f) w
+let updateij f w = List.mapi (fun i -> List.mapi (fun j -> f i j)) w
 
 let neighbors w i j =
   [
@@ -30,18 +36,27 @@ let neighbors w i j =
     [ peek w (i + 1) (j - 1); peek w (i + 1) j; peek w (i + 1) (j + 1) ];
   ]
 
+let neighbors_pos w i j =
+  let r0 = List.nth w 0 in
+  let m = List.length w in
+  let n = List.length r0 in
+  if i >= 0 && j >= 0 && i < m && j < n then
+    List.filter
+      (fun (i', j') -> i' >= 0 && j' >= 0 && i' < m && j' < n)
+      [
+        (i - 1, j - 1);
+        (i - 1, j);
+        (i - 1, j + 1);
+        (i, j - 1);
+        (i, j + 1);
+        (i + 1, j - 1);
+        (i + 1, j);
+        (i + 1, j + 1);
+      ]
+  else []
+
 let neighbors_flat w i j =
-  [
-    peek w (i - 1) (j - 1);
-    peek w (i - 1) j;
-    peek w (i - 1) (j + 1);
-    peek w i (j - 1);
-    peek w i j;
-    peek w i (j + 1);
-    peek w (i + 1) (j - 1);
-    peek w (i + 1) j;
-    peek w (i + 1) (j + 1);
-  ]
+  List.map (fun (i', j') -> peek w i' j') (neighbors_pos w i j)
 
 let bomb_nb w i j =
   let ns = neighbors_flat w i j in
@@ -51,26 +66,66 @@ let bomb_nb w i j =
       + acc)
     0 ns
 
-let rnd_world ~p ~height ~width =
-  List.init height (fun _ ->
-      List.init width (fun _ -> if Random.int 100 < p then Bomb else Free 0))
-
-let step w =
-  List.mapi
-    (fun i r ->
-      List.mapi
-        (fun j -> function Bomb -> Bomb | Free _ -> Free (bomb_nb w i j))
-        r)
+let unseal1 w i j =
+  updateij
+    (fun i' j' c ->
+      match c with
+      | (New cell | Sealed cell) when i = i' && j = j' -> Unsealed cell
+      | _ -> c)
     w
 
-let gen_world ?(p = 5) ?(height = 20) ?(width = 20) () =
+let seal w i j =
+  updateij
+    (fun i' j' c ->
+      match c with New cell when i = i' && j = j' -> Sealed cell | _ -> c)
+    w
+
+let rec unseal w i j =
+  match peek w i j with
+  | None | Some (Unsealed _) -> w
+  | Some (New cell | Sealed cell) -> (
+      let w' = unseal1 w i j in
+      let ns = neighbors_pos w i j in
+      match cell with
+      | Safe 0 -> List.fold_left (fun acc (i', j') -> unseal acc i' j') w' ns
+      | _ -> w')
+
+let seal_input w i j =
+  match peek w i j with
+  | None -> Error "wrong selection"
+  | Some (New _) -> Ok (seal w i j, Continue)
+  | Some (Sealed _ | Unsealed _) -> Error "cannot seal again!"
+
+let unseal_input w i j =
+  match peek w i j with
+  | None -> Error "wrong selection"
+  | Some (New Mined | Sealed Mined) -> Ok (unseal w i j, Over)
+  | Some (New _ | Sealed _) -> Ok (unseal w i j, Continue)
+  | Some (Unsealed _) -> Error "cannot unseal again!"
+
+let rnd_world ~p ~height ~width =
+  List.init height (fun _ ->
+      List.init width (fun _ -> if Random.int 100 < p then Mined else Safe 0))
+
+let step w =
+  updateij
+    (fun i j -> function Mined -> Mined | Safe _ -> Safe (bomb_nb w i j))
+    w
+
+let gen_world ?(p = 5) ?(height = 5) ?(width = 5) () =
   let w = rnd_world ~p ~height ~width in
   step w
+
+let gen_field () =
+  let w = gen_world () in
+  update (fun c -> New c) w
 
 let attrs_of_cell =
   let open T in
   function
-  | Bomb -> [ red; on_red ] | Free 0 -> [ on_cyan ] | Free _ -> [ blue; on_cyan ]
+  | Mined -> [ red; on_red ]
+  | Safe 0 -> [ on_cyan ]
+  | Safe _ -> [ blue; on_cyan ]
 
 let print_cell c = T.printf (attrs_of_cell c) "%s" (string_of_cell c)
 
--- test/minesweepertest.ml
+++ test/minesweepertest.ml
@@ -10,8 +10,12 @@ let w =
   ]
 
 let%test _ =
-  neighbors_pos 2 2
+  neighbors_pos w 2 2
   = [ (1, 1); (1, 2); (1, 3); (2, 1); (2, 3); (3, 1); (3, 2); (3, 3) ]
+
+let%test _ = neighbors_pos w 0 0 = [ (0, 1); (1, 0); (1, 1) ]
+let%test _ = neighbors_pos w (-1) 0 = []
+let%test _ = neighbors_pos w 5 5 = []
 
 let%test _ =
   neighbors w 2 2
@@ -37,9 +41,59 @@ let%test _ =
       [ None; Some (Safe 1); Some (Safe 1) ];
     ]
 
-let%test _ = bomb_nb w 0 0 = 1 (* includes bomb at i,j *)
+let%test _ = bomb_nb w 0 0 = 0 (* excludes bomb at i,j *)
 let%test _ = bomb_nb w 2 2 = 2
 let%test _ = bomb_nb w 0 5 = 0
-let%test _ = bomb_nb w 2 3 = 2
-let%test _ = bomb_nb w 3 2 = 2
+let%test _ = bomb_nb w 2 3 = 1
+let%test _ = bomb_nb w 3 2 = 1
 let%test _ = bomb_nb w 2 2 = 2
+
+let w = update (fun c -> New c) w
+
+let%test _ = unseal w 0 3 =   [
+  [ New(Mined ); Unsealed(Safe 1); Unsealed(Safe 0); Unsealed(Safe 0); Unsealed(Safe 0) ];
+  [ New(Safe 1); Unsealed(Safe 1); Unsealed(Safe 1); Unsealed(Safe 1); Unsealed(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 2); New(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); New(Safe 2); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 0) ];
+]
+
+let%test _ = unseal w 4 4 =   [
+  [ New(Mined ); New(Safe 1); New(Safe 0); New(Safe 0); New(Safe 0) ];
+  [ New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 2); New(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); Unsealed(Safe 2); Unsealed(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); Unsealed(Safe 1); Unsealed(Safe 0) ];
+]
+
+let%test _ = unseal w 2 2 =   [
+  [ New(Mined ); New(Safe 1); New(Safe 0); New(Safe 0); New(Safe 0) ];
+  [ New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); Unsealed(Safe 2); New(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); New(Safe 2); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 0) ];
+]
+
+let%test _ = unseal w 2 3 =   [
+  [ New(Mined ); New(Safe 1); New(Safe 0); New(Safe 0); New(Safe 0) ];
+  [ New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 2); Unsealed(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); New(Safe 2); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 0) ];
+]
+
+let%test _ = seal w 2 3 =   [
+  [ New(Mined ); New(Safe 1); New(Safe 0); New(Safe 0); New(Safe 0) ];
+  [ New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 2); Sealed(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); New(Safe 2); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 0) ];
+]
+
+let%test _ = seal w 5 3 =   [
+  [ New(Mined ); New(Safe 1); New(Safe 0); New(Safe 0); New(Safe 0) ];
+  [ New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 2); New(Mined ); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Mined ); New(Safe 2); New(Safe 1) ];
+  [ New(Safe 0); New(Safe 1); New(Safe 1); New(Safe 1); New(Safe 0) ];
+]
